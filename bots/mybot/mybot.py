2f90f79d1e36a3947261ed35b2c4704b7799f22f
Mark Melnic <markhappy200@gmail.com>
2020-11-22 14:51:19 +0100

    Implemented my bot

--- bots/mybot/mybot.py
+++ bots/mybot/mybot.py
@@ -14,22 +14,16 @@ class Bot:
         pass
 
     def get_move(self, state):
-        # type: (State) -> tuple[int, int]
-        """
-        Function that gets called every turn. This is where to implement the strategies.
-        Be sure to make a legal move. Illegal moves, like giving an index of a card you
-        don't own or proposing an illegal mariage, will lose you the game.
-       	TODO: add some more explanation
-        :param State state: An object representing the gamestate. This includes a link to
-            the states of all the cards, the trick and the points.
-        :return: A tuple of integers or a tuple of an integer and None,
-            indicating a move; the first indicates the card played in the trick, the second a
-            potential spouse.
-        """
-
         # All legal moves
         moves = state.moves()
 
+        if moves:
+            try:
+                moves.sort(key=lambda move:move[0])
+                max_move = max(moves)
+            except:
+                max_move = moves[-1]
+        else:
+            max_move = random.choice(moves)
 
-        # Return a random choice
-        return random.choice(moves)
+        return max_move
